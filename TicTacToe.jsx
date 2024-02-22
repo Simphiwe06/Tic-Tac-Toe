3de523e44f3ba79882db84bd7f8d897061e56b36
derekMgwena <derekmakhubela@gmail.com>
2024-02-22 18:51:23 +0200

    fixed errors

--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react' 6.9k (gzipped: 2.7k)
+import React, { useRef, useState } from 'react'
 import './TicTacToe.css'
 import circle_icon from '../Assets/circle.png';
 import cross_icon from "../Assets/cross.png";
@@ -84,16 +84,16 @@ const TicTacToe = () => {
         setlock(true);
         if(winner==="x")
         {
-            titleRef.current.innerHTML = `Congratultions: <img src=>${cross_icon}`   
+            titleRef.current.innerHTML = `Congratultions: <img src=${cross_icon}> Wins!`   
         }
         else 
         {
-            titleRef.current.innerHTML = `Congratultions: <img src=>${circle_icon}` 
+            titleRef.current.innerHTML = `Congratultions: <img src=${circle_icon}> Wins!` 
         }
     }
 
     const reset =() => {
-        setLock(false);
+        setlock(false);
         data = ["", "", "", "", "", "", "", "", ""]
         titleRef.current.innerHTML = 'Tic Tac Toe In <span>React</span>';
         box_array.map((e)=>{
@@ -106,19 +106,19 @@ const TicTacToe = () => {
       <h1 className='title' ref={titleRef}>Tic Tac Toe Game In <span>React</span></h1>
       <div className='board'>
         <div className='row1'>
-            <div className='boxes' ref={box1} onClick={(e)=>{toogle(e,0)}}></div>
-            <div className='boxes' ref={box2} onClick={(e)=>{toogle(e,1)}} ></div>
-            <div className='boxes' ref={box3}  onClick={(e)=>{toogle(e,2)}} ></div>
+            <div className='boxes' ref={box1} onClick={(e)=>{toggle(e,0)}}></div>
+            <div className='boxes' ref={box2} onClick={(e)=>{toggle(e,1)}} ></div>
+            <div className='boxes' ref={box3} onClick={(e)=>{toggle(e,2)}} ></div>
         </div>
         <div className='row2'>
-            <div className='boxes' ref={box4} onClick={(e)=>{toogle(e,3)}}></div>
-            <div className='boxes' ref={box5} onClick={(e)=>{toogle(e,4)}}></div>
-            <div className='boxes' ref={box6} onClick={(e)=>{toogle(e,5)}}></div>
+            <div className='boxes' ref={box4} onClick={(e)=>{toggle(e,3)}}></div>
+            <div className='boxes' ref={box5} onClick={(e)=>{toggle(e,4)}}></div>
+            <div className='boxes' ref={box6} onClick={(e)=>{toggle(e,5)}}></div>
         </div>
         <div className='row3'>
-            <div className='boxes' ref={box7} onClick={(e)=>{toogle(e,6)}}></div>
-            <div className='boxes' ref={box8} onClick={(e)=>{toogle(e,7)}}></div>
-            <div className='boxes' ref={box9} onClick={(e)=>{toogle(e,8)}}></div>
+            <div className='boxes' ref={box7} onClick={(e)=>{toggle(e,6)}}></div>
+            <div className='boxes' ref={box8} onClick={(e)=>{toggle(e,7)}}></div>
+            <div className='boxes' ref={box9} onClick={(e)=>{toggle(e,8)}}></div>
         </div>
       </div>
       <button className='reset' onClick={()=>{reset()}}>Reset</button>
